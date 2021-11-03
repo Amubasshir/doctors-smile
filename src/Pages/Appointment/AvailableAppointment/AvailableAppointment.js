ee427bf17b03ebb7926448aabe0c1cf2ab438fac
Amubasshir <lastmubasshir@gmail.com>
2021-11-03 11:52:05 +0600

    appointment added

---
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvailableAppointment = ({ date }) => {
+  return (
+    <div>
+      <h2>AvailableAppointment: {date.toDateString()}</h2>
+    </div>
+  );
+};
+
+export default AvailableAppointment;
