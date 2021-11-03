60489ce893db33905ab961e0ed0d56cea3ed70f4
Amubasshir <lastmubasshir@gmail.com>
2021-11-03 20:11:35 +0600

    client appointment form  done

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,4 +1,4 @@
-import { Fade, Modal, Typography } from '@mui/material';
+import { Button, Fade, Modal, Typography } from '@mui/material';
 import Backdrop from '@mui/material/Backdrop';
 import TextField from '@mui/material/TextField';
 import { Box } from '@mui/system';
@@ -10,14 +10,25 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgColor: 'white',
+  bgColor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
 
-const BookingModal = ({ openBooking, handleBookingClose, booking }) => {
+const BookingModal = ({ openBooking, handleBookingClose, booking, date }) => {
   const { name, time } = booking;
+
+  const handleBookingSubmit = (e) => {
+    alert('submitting');
+
+    //   collect data
+    //   send to the server
+
+    handleBookingClose();
+    e.preventDefault();
+  };
+
   return (
     <Modal
       aria-labelledby="transition-modal-title"
@@ -31,17 +42,53 @@ const BookingModal = ({ openBooking, handleBookingClose, booking }) => {
       }}
     >
       <Fade in={openBooking}>
-        <Box sx={style}>
+        <Box style={{ backgroundColor: 'white' }} sx={style}>
           <Typography id="transition-modal-title" variant="h6" component="h2">
             {name}
           </Typography>
-          <form>
+          <form
+            onSubmit={handleBookingSubmit}
+            style={{ backgroundColor: 'white' }}
+          >
             <TextField
-              label="Size"
+              disabled
+              sx={{ width: '90%', margin: 1 }}
               id="outlined-size-small"
-              defaultValue="Small"
+              defaultValue={time}
               size="small"
             />
+            <TextField
+              sx={{ width: '90%', margin: 1 }}
+              id="outlined-size-small"
+              defaultValue="Your Name"
+              size="small"
+            />
+            <TextField
+              sx={{ width: '90%', margin: 1 }}
+              id="outlined-size-small"
+              defaultValue="Your Email"
+              size="small"
+            />
+            <TextField
+              sx={{ width: '90%', margin: 1 }}
+              id="outlined-size-small"
+              defaultValue="Your Phone-number"
+              size="small"
+            />
+            <TextField
+              disabled
+              sx={{ width: '90%', margin: 1 }}
+              id="outlined-size-small"
+              defaultValue={date.toDateString()}
+              size="small"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              style={{ backgroundColor: '#5cb85c' }}
+            >
+              Book
+            </Button>
           </form>
         </Box>
       </Fade>
