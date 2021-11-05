ce7701a272fc07f3c281811355d0c11225e61750
Amubasshir <lastmubasshir@gmail.com>
2021-11-05 11:39:09 +0600

    login field

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,13 +1,56 @@
-import { Container, Grid } from '@mui/material';
-import React from 'react';
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
 import login from '../../../images/login.png';
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({});
+
+  const handleOnChange = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newLoginData = { ...loginData };
+    newLoginData[field] = value;
+    setLoginData(newLoginData);
+  };
+
+  const handleLoginSubmit = (e) => {
+    alert('hello');
+    e.preventDefault();
+  };
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <h2>welcome to login</h2>
+        <Grid sx={{ mt: 8 }} item xs={12} md={6}>
+          <Typography variant="body1" gutterBottom>
+            login
+          </Typography>
+          <form onSubmit={handleLoginSubmit}>
+            <TextField
+              sx={{ width: '75%', m: 1 }}
+              id="standard-basic"
+              label="Your Email"
+              name="email"
+              onChange={handleOnChange}
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: '75%', m: 1 }}
+              id="standard-basic"
+              label="Your Password"
+              type="password"
+              name="password"
+              onChange={handleOnChange}
+              variant="standard"
+            />
+            <Button
+              sx={{ width: '75%', m: 1 }}
+              type="submit"
+              variant="contained"
+            >
+              {' '}
+              Login
+            </Button>
+          </form>
         </Grid>
         <Grid item xs={4} md={6}>
           <img style={{ width: '100%' }} src={login} alt="" />
